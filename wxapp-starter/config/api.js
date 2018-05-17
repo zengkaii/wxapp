69bdcfe1656ad711455be40242166d7ddb34d2b3
zengkai <1965599425@qq.com>
2018-05-17 23:36:26 +0800

    修改了wxapp-starter

--- wxapp-starter/config/api.js
+++ wxapp-starter/config/api.js
@@ -1,1 +1,2 @@
-export const API_BASE = 'https://www.easy-mock.com/mock/5afc29f9e5c64d22cc1ca49e/start/start#!method=get'
+export const API_BASE = 'https://www.easy-mock.com/mock/5afc29f9e5c64d22cc1ca49e/start/start#!method=get';
+export const API_CREATE= 'https://www.easy-mock.com/mock/5af03ac5957fd4308a30b9c0/api/create#!method=get';
